2b9b6ef6aabe088b29596c6d34a6a9093d888f7a
Ikromjon7218 <ikromjon7218@gmail.com>
2023-04-17 17:48:25 +0500

    requirement.txt

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,13 @@
-from django.contrib import admin
+from rest_framework.views import exception_handler
 from django.urls import path, include
 from asosiy.views import *
 from rest_framework.routers import DefaultRouter
+from rest_framework.response import Response
+
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -21,9 +22,8 @@ schema_view = get_schema_view(
 router = DefaultRouter()
 router.register('plan', PlanViewSet)
 
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', schema_view.with_ui("swagger", cache_timeout=0)),
     path('', include(router.urls)),
-
 ]
